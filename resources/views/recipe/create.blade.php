2754cf13355244102bf78e6854321f116cfcce68
ShoContain <perblue.666@gmail.com>
2022-09-10 20:05:45 +0900

    change: 説明のフォームに名前のエラーメッセージが出てたので修正

--- resources/views/recipe/create.blade.php
+++ resources/views/recipe/create.blade.php
@@ -17,16 +17,16 @@
         <label for="description">説明</label>
         <textarea name="description" class="form-control" id="description" rows="3"></textarea>
         @if ($errors->has('description'))
-          <p class="text-danger">{{$errors->first('name')}}</p>
+          <p class="text-danger">{{$errors->first('description')}}</p>
         @endif
       </div>
     </div>
     <div class="form-row">
       <div class="form-group col-md-4 mb-3">
-        <label for="calories">カロリー</label>
-        <input name="calories" class="form-control" id="calories">
-        @if ($errors->has('calories'))
-          <p class="text-danger">{{$errors->first('calories')}}</p>
+        <label for="kcal">カロリー</label>
+        <input name="kcal" class="form-control" id="kcal">
+        @if ($errors->has('kcal'))
+          <p class="text-danger">{{$errors->first('kcal')}}</p>
         @endif
       </div>
       </div>
